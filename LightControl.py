81ebff558a9e1bc8b3bae92d8412ff98dca55de6
Daniel Galron <daniel.galron@gmail.com>
2014-02-18 21:01:03 -0500

    Stubbed out light control API

---
+++ LightControl.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request, abort
+
+app = Flask(__name__)
+
+
+@app.route('/api/power', methods=['POST'])
+def set_power():
+    if not request.json or 'power_state' not in request.json:
+        abort(400)
+    return jsonify({'power': request.json['power_state'], 'state_did_change': True})
+
+
+@app.route('/api/dim', methods=['POST'])
+def set_dimness():
+    if not request.json or 'intensity' not in request.json:
+        abort(400)
+    return jsonify({'intensity': request.json['intensity'], 'state_did_change': True})
+
+
+@app.route('/api/pattern', methods=['POST'])
+def set_pattern():
+    if not request.json or 'pattern' not in request.json:
+        abort(400)
+    return jsonify({'pattern': request.json['pattern'], 'state_did_change': True})
+
+
+@app.route('/api/light_texture', methods=['POST'])
+def set_light_texture():
+    if not request.json or 'texture' not in request.json:
+        abort(400)
+    return jsonify({'texture': request.json['texture'], 'state_did_change': True})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
