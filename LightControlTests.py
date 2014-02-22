c5356b3c1b6f47cbdade8cba5749dd24caff1ab9
Clair Seager <cseager@gmail.com>
2014-02-21 23:41:45 -0500

    git stuff

--- LightControlTests.py
+++ LightControlTests.py
@@ -17,5 +17,6 @@ class LightControlTester(unittest.TestCase):
         p = self.app.post('/api/power', data=json.dumps({'power_state': 1}), content_type='application/json')
         self.assertDictEqual(json.loads(p.data), {"power": 1, "state_did_change": True})
 
+
 if __name__ == '__main__':
     unittest.main()
