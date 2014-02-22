f736b0fdebe7a6bd7f014bde28de126758e0b8c1
Daniel Galron <daniel.galron@gmail.com>
2014-02-22 00:10:48 -0500

    Finalized test_power unit test

--- LightControlTests.py
+++ LightControlTests.py
@@ -1,6 +1,7 @@
 __author__ = 'galron'
 
 import json
+import time
 import unittest
 
 import LightControl
@@ -13,10 +14,12 @@ class LightControlTester(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_set_power(self):
+    def test_power(self):
         p = self.app.post('/api/power', data=json.dumps({'power_state': 1}), content_type='application/json')
         self.assertDictEqual(json.loads(p.data), {"power": 1, "state_did_change": True})
-
+        time.sleep(10)
+        p = self.app.post('/api/power', data=json.dumps({'power_state': 0}), content_type='application/json')
+        self.assertDictEqual(json.loads(p.data), {"power": 0, "state_did_change": True})
 
 if __name__ == '__main__':
     unittest.main()
